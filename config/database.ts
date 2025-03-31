dd73356ec8a950992632c3edc06af3e9c7d79c49
rhenardM <rhenardmunongo@gmail.com>
2025-03-31 21:50:43 +0100

    feat: run migrations for all models on local databases

--- config/database.ts
+++ config/database.ts
@@ -1,4 +1,4 @@
-// import env from '#start/env'
+import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
@@ -7,11 +7,11 @@ const dbConfig = defineConfig({
     postgres: {
       client: 'pg',
       connection: {
-        host: process.env.DB_HOST || '', // Mettre une valeur vide par défaut
-        port: Number(process.env.DB_PORT) || 5432, // Valeur par défaut PostgreSQL
-        user: process.env.DB_USER || '',
-        password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_DATABASE || '',
+        host: env.get('DB_HOST', '127.0.0.1'),
+        port: parseInt(env.get('DB_PORT', '5432')),        
+        user: env.get('DB_USER', 'user'),
+        password: env.get('DB_PASSWORD', 'Hack2025'),
+        database: env.get('DB_DATABASE', 'tweeter_clone_with_adonis'),
       },
       migrations: {
         naturalSort: true,
