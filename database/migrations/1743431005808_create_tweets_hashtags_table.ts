f62d1751c303e7ed43776c121b9934b2ab250102
rhenardM <rhenardmunongo@gmail.com>
2025-03-31 15:25:05 +0100

    (feat): add table pivot tweets_hashtags

---
+++ database/migrations/1743431005808_create_tweets_hashtags_table.ts
@@ -0,0 +1,19 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'tweets_hashtags'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('tweet_id').unsigned().references('tweets.id').onDelete('CASCADE')
+      table.integer('hashtag_id').unsigned().references('hashtags.id').onDelete('CASCADE')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
